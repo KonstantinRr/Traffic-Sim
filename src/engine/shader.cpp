330026cd4ad03778e2f7d2a4b9a49fa514529483
Konstantin Rolf <konstantin.rolf@gmail.com>
2020-09-23 23:35:04 +0200

    Fixed Layout locations

--- src/engine/shader.cpp
+++ src/engine/shader.cpp
@@ -655,8 +655,8 @@ std::vector<char> LineMemoryShader::retrieveVertexShader()
 
     uniform mat4 mvp;
 
-    in vec2 vVertex;
-    in vec3 color;
+    layout (location = 0) in vec2 vVertex;
+    layout (location = 1) in vec3 color;
     out vec3 mixedColor;
 
     void main(void) {
